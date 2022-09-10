1521c471b92d3c70cc7adef54b1aff576dbfa341
Misir Jafarov <misir.ceferov@gmail.com>
2022-09-10 11:23:19 +0400

    Update key validation regexp

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-const { createServer } = require('http');
-const { clearInterval } = require('timers');
+const { createServer } = require("http");
+const { clearInterval } = require("timers");
 const { PORT, TTL } = process.env;
 const port = parseInt(PORT) || 8080;
 const ttl = (parseInt(TTL) || 180) * 1000;
 const locks = {};
 
 createServer(function (req, res) {
-  const match = req.url.match(/[a-zA-Z0-9]+/g);
-  const key = match && match[0];
+  const match = /^\/?([a-zA-Z0-9_.-]{3,16})\/?$/g.exec(req.url);
+  const key = match && match[1];
 
-  if (!key || key.length < 2 || key.length > 15) {
+  if (!key) {
     res.statusCode = 400;
     res.end();
     return;
   }
 
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const interval = setInterval(function () {
       if (!locks[key] || locks[key] < Date.now()) {
         locks[key] = Date.now() + ttl;
-        
+
         clearInterval(interval);
         res.statusCode = 200;
         res.end();
       }
     }, 500);
 
-    res.on('close', function () {
+    res.on("close", function () {
       clearInterval(interval);
     });
-  } else if (req.method === 'DELETE') {
+  } else if (req.method === "DELETE") {
     delete locks[key];
     res.statusCode = 200;
     res.end();
